c00702cc3f80b5bfb7306fc20a9b4ff3f8c9dfd4
Alban <marguet.alban@gmail.com>
2026-07-17 11:53:39 +0200

    add tmux + nvim smart splits

--- nvim/lua/config/keymap.lua
+++ nvim/lua/config/keymap.lua
@@ -2,3 +2,18 @@
 -- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 --
 -- vim.keymap.set("n", "<leader>rr", "<cmd>!bin/rails routes<cr>", { desc = "Rails routes" })
+vim.keymap.set("n", "<leader><delete>", function()
+  Snacks.bufdelete()
+end, { desc = "Close Buffer" })
+vim.keymap.set("n", "<leader><CR>", "<cmd>%bd<cr>", { desc = "Close All Buffers" })
+
+-- smart-splits.nvim: override LazyVim's default <C-hjkl> window nav so it also
+-- crosses into tmux panes. Plugin loads eagerly (see plugins/smart-splits.lua),
+-- so require() here is safe by the time this file runs on VeryLazy.
+local ok, smart_splits = pcall(require, "smart-splits")
+if ok then
+  vim.keymap.set("n", "<C-h>", smart_splits.move_cursor_left, { desc = "Move to left split/pane" })
+  vim.keymap.set("n", "<C-j>", smart_splits.move_cursor_down, { desc = "Move to below split/pane" })
+  vim.keymap.set("n", "<C-k>", smart_splits.move_cursor_up, { desc = "Move to above split/pane" })
+  vim.keymap.set("n", "<C-l>", smart_splits.move_cursor_right, { desc = "Move to right split/pane" })
+end
---
+++ nvim/lua/plugins/smart-splits.lua
@@ -0,0 +1,5 @@
+return {
+  "mrjones2014/smart-splits.nvim",
+  lazy = false, -- must load eagerly: sets the tmux @pane-is-vim variable on load
+  opts = {},
+}
