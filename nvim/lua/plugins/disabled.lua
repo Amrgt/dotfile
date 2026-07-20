188a8df6893ee0e2dbba83aeb82712630912d9a8
Alban <marguet.alban@gmail.com>
2026-07-20 14:59:01 +0200

    fix split and option-h

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -10,6 +10,17 @@ return {
       explorer = {
         enabled = false,
       },
+      -- aerospace grabs <M-h> globally, so rebind toggle_hidden to <C-h>
+      -- (Ghostty's kitty keyboard protocol keeps it distinct from <BS>)
+      picker = {
+        win = {
+          input = {
+            keys = {
+              ["<c-h>"] = { "toggle_hidden", mode = { "i", "n" } },
+            },
+          },
+        },
+      },
     },
   },
 }
